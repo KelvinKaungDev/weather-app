dc72189680034bc2f5823f2ee4c307287ef061b4
KelvinKaungDev <u6511753@au.edu>
2022-11-20 23:49:22 +0700

    Change Navbar

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,54 +1,36 @@
 <template>
     <div>
-        <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 pt-5 pb-5">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://flowbite.com/" class="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Kelvin Weather Focus</span>
                 </a>
-                <div class="flex md:order-2">
-                    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Search</span>
-                    </button>
+                <div class="flex md:order-1 relative">
+                    <div class="absolute -inset-1 bg-gradient-to-r from-blue-800 to-blue-700 rounded-lg-blur-xl blur"></div>
 
-                    <div class="hidden relative md:block">
+                    <div class="relative md:block">
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                             <span class="sr-only">Search icon</span>
                         </div>
-                        <input @keyup.enter="search()" v-model="inputData" type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+                        <input @keyup.enter="search()" v-model="inputData" type="text" id="search-navbar" class="w-[700px] block p-2 pl-10 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
                     </div>
-
-                    <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-                        <span class="sr-only">Open menu</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    </button>
                 </div>
 
-                <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-search">
-                    <div class="relative mt-3 md:hidden">
-                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                <div class="flex md:order-2 text-gray-500 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                    </svg>
+                    <div class="ml-3">
+                        Dark
                     </div>
-                    <input type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
-                    </div>
-                    <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                    </li>
-                    </ul>
                 </div>
+
             </div>
         </nav>
 
-        <div class="grid w-full md:grid-cols-2 mt-14 justify-items-center">
+        <div class="grid w-full md:grid-cols-2 mt-20 justify-items-center">
 
             <div class="border-2 border-white-500 p-8" v-if="showData">
                 <p class="text-5xl mt-20 text-white text-center">
@@ -71,7 +53,7 @@
 
             </div>
 
-            <div class="p-8" v-else>
+            <div class="p-8 animate-slide" v-else>
                 <p class="text-4xl mt-28 text-white text-center">
                     Welcome to <span class="border-b-4">Kelvin Weather Focus</span>
                 </p>
@@ -83,16 +65,44 @@
             </div>
 
             <div class="scale-75 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-90 duration-300">
-                <a href="#">
-                    <img class="rounded-t-lg" src="../img/sunny.jpg" alt="">
-                </a>
+                <div v-if="condition.includes('clouds')">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/cloudy.jpg" alt="">
+                    </a>
+                </div>
+
+                <div v-else-if="condition.includes('sky')">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/clear_sky.jpg" alt="">
+                    </a>
+                </div>
+
+                <div v-else-if="condition.includes('rain')">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/raining.jpg" alt="">
+                    </a>
+                </div>
+
+                <div v-else-if="condition.includes('sunny')">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/sunny.jpg" alt="">
+                    </a>
+                </div>
+
+                <div v-else-if="condition.includes('snowing')">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/sunny.jpg" alt="">
+                    </a>
+                </div>
+
+                <div v-else>
+                    <a href="#">
+                        <img class="rounded-t-lg" src="../img/weather_focus.jpg" alt="">
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-
-
-
-
 
 </template>
 
@@ -121,8 +131,8 @@ import axios from 'axios';
                 .then(response => {
                     const data = response.data;
 
-                    this.cityName = data.name;
-                    this.countryName    = data.sys.country;
+                    this.cityName    = data.name;
+                    this.countryName = data.sys.country;
                     this.temperature = Math.round(data.main.temp);
                     this.condition   = data.weather[0].description;
                     this.lowTemp     = Math.round(data.main.temp_min);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,21 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        animation : {
+            slide : 'slide 4s infinite'
+        },
+        keyframes : {
+            slide : {
+                from :  {
+                    color : 'yellow'
+                },
+                to : {
+                    color : 'green'
+                }
+            }
+        }
+      },
     },
     variants: {
       extend: {},
